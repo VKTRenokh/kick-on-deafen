f207e7cbc961bc595f6de594698761075e118474
Roman <40242499+PoMaKoM@users.noreply.github.com>
2024-04-17 21:03:42 +0200

    Update main.ts - DO NOT MERGE

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,3 @@
 import { bot } from './bot'
 
-export const main = () => {
-  if (!process.env.TOKEN) {
-    console.error('no token was provided')
-    process.exit(1)
-  }
 
-  bot(process.env.TOKEN)
-}
