4404979d06e819d3ba17fec16b9749e56ac03b19
VKTRenokh <viktorenokh@gmail.com>
2024-04-18 00:15:22 +0300

    feat: add newline

--- src/bot/voice/constants/whitelist.ts
+++ src/bot/voice/constants/whitelist.ts
@@ -1,5 +1,6 @@
 export const whitelist = [
   '884100077344419861', // Jirisimonov
   '1064595756515852419', // Gentlemen
-  '282509088875872258' // intelligentRaji
+  '282509088875872258', // intelligentRaji
 ]
+
